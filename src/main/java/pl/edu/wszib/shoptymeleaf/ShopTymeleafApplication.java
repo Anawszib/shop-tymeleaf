ad1b87bfb807e0c7897fe38218774b3ff46476ab
Anna Adamik <quenthel5@gmail.com>
2021-04-10 10:14:49 +0200

    Inicjalizacja projektu

---
+++ src/main/java/pl/edu/wszib/shoptymeleaf/ShopTymeleafApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.shoptymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopTymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopTymeleafApplication.class, args);
+    }
+
+}
